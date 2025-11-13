b8f918b0b03f521ab224e243f0452a73c308fb16
EvaSerra <eserrasierra@gmail.com>
2025-11-12 17:06:30 -0800

    Minor Styling Issues Corrected

--- src/main/java/edu/sdccd/cisc/Main.java
+++ src/main/java/edu/sdccd/cisc/Main.java
@@ -1,7 +1,5 @@
 package edu.sdccd.cisc;
 
-//TODO: import javafx libraries
-
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -12,42 +10,35 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        // TODO: title
 
+        Label header = new Label("Message Board");
+        header.setFont(new Font(20));
+        header.setStyle("-fx-font-weight: bold");
+        header.setAlignment(Pos.CENTER);
 
-        // TODO: header
-        Label Header = new Label();
-        // TODO: status label
         Label status = new Label("Ready");
-        // TODO: Input row: TextField + Add button
         TextField message = new TextField();
         message.setPromptText("Enter Message");
-        // TODO: prompt for message
         Label prompt = new Label("Write your message: ");
-        // TODO: add button
         Button addBtn = new Button("Add");
-        // TODO: reset button
         Button resetBtn = new Button("Reset");
-        // TODO: message history area
         TextArea messagesLog = new TextArea();
         messagesLog.setEditable(false);
 
         Label wordCount = new Label("");
-        // TODO: Wire up the Add button.
         // - If input text is empty or blank: set status to "Nothing to add" and return.
         // - Otherwise: append text to history (one line per message),
         //              clear the input field,
         //              set status to "Last action: added message".
 
         addBtn.setOnAction(e -> {
-            // TODO: implement
             if (message.getText().isBlank()) {
                 status.setText("Nothing to add");
                 message.setText("");
@@ -58,28 +49,25 @@ public class Main extends Application {
                 message.setText("");
             }
         });
-        // TODO: Wire up the Reset button.
         // - Clear history and input.
         // - Set status to "Cleared".
         resetBtn.setOnAction(e -> {
-            // TODO: implement
             messagesLog.setText("");
             message.setText("");
             status.setText("Last Action: cleared board");
             wordCount.setText("");
         });
 
-        // TODO: add content to root
         HBox buttonBar = new HBox( 40, addBtn, resetBtn);
         buttonBar.setAlignment(Pos.CENTER);
-        VBox messagesBox =  new VBox(20, messagesLog, message, wordCount);
+        VBox topContainer = new VBox(20, header, status);
+        VBox messagesBox =  new VBox(20, messagesLog, prompt, message, wordCount);
         messagesBox.setAlignment(Pos.CENTER);
         BorderPane root = new BorderPane();
-        root.setTop(status);
+        root.setTop(topContainer);
         root.setCenter(messagesBox);
         root.setBottom(buttonBar);
 
-        // TODO: set scene and show stage
         Scene scene = new Scene(root, 450, 300);
         stage.setMinWidth(400);
         stage.setMinHeight(250);
